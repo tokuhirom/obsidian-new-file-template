bf506285d8960e7759d8e14b56fcb830156815db
Tokuhiro Matsuno <tokuhirom@gmail.com>
2021-04-27 17:52:59 +0900

    implemented

--- main.ts
+++ main.ts
@@ -1,105 +1,105 @@
-import {App, moment, Plugin, PluginSettingTab, Setting} from 'obsidian';
+import {App, MarkdownView, moment, Plugin, PluginSettingTab, Setting, TFile} from "obsidian";
 
-interface MyPluginSettings {
-	template: string;
+interface NewFileTemplatePluginSettings {
+  template: string;
 }
 
-const DEFAULT_TEMPLATE = (
-		"---\n" +
-		"date: {{date:YYYY-MM-DD}}\n" +
-		"aliases: []\n" +
-		"---\n"
-);
+const DEFAULT_TEMPLATE =
+  `---
+date: {{date:YYYY-MM-DD}}
+aliases: []
+---
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	template: DEFAULT_TEMPLATE
+
+
+`;
+
+const DEFAULT_SETTINGS: NewFileTemplatePluginSettings = {
+  template: DEFAULT_TEMPLATE,
+};
+
+export default class NewFileTemplatePlugin extends Plugin {
+  settings: NewFileTemplatePluginSettings;
+
+  async onload() {
+    console.log("loading new-file-template plugin");
+
+    await this.loadSettings();
+
+    this.addSettingTab(new NewFileTemplatePluginSettingTab(this.app, this));
+
+    this.app.workspace.on("file-open", this.onFileOpen.bind(this))
+  }
+
+  async onFileOpen(file: TFile) {
+    if (file == null) {
+      return;
+    }
+
+    const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+    if (markdownView == null) {
+      return;
+    }
+
+    const content = await file.vault.read(file)
+    if (content == "" && markdownView.getMode() == "source") {
+      const content = this.renderTemplate(this.settings.template)
+      await markdownView.sourceMode.set(content, false)
+
+      await markdownView.editor.focus()
+      await markdownView.editor.setCursor({line: content.split(/\n/).length, ch: 1})
+      // same as `workspace:edit-file-title`
+      await this.app.workspace.getActiveViewOfType(MarkdownView)?.leaf?.setEphemeralState({rename:"all"})
+    }
+  }
+
+  renderTemplate(v: string) {
+    return v.replace(/{{\s*date\s*:\s*(.*?)}}/gi, (_, fmt) => {
+      return moment().format(fmt);
+    });
+  }
+
+  onunload() {
+    console.log("unloading new-file-templatte plugin");
+  }
+
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+class NewFileTemplatePluginSettingTab extends PluginSettingTab {
+  plugin: NewFileTemplatePlugin;
 
-	async onload() {
-		console.log('loading new-file-template plugin');
+  constructor(app: App, plugin: NewFileTemplatePlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-		await this.loadSettings();
+  display(): void {
+    let { containerEl } = this;
 
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+    containerEl.empty();
 
-		this.addCommand({
-			id: 'new-file-with-template',
-			name: 'Create new file with template',
-			callback: this.createNewFile.bind(this)
-		});
-
-		// this.registerEvent(this.app.vault.on("create", this.onFileCreated.bind(this)));
-	}
-
-	async createNewFile() : Promise<boolean> {
-			console.log("Create new file with template!!")
-			const data = this.renderTemplate(this.settings.template);
-			for (let i=0; i<1000; i++) {
-				const path = i==0 ? "Untitled.md" : "Untitled " + i + ".md";
-				try {
-					await this.app.vault.create(path, data);
-				} catch (e) {
-					console.log(path);
-					console.log(e);
-					continue;
-				}
-				await this.app.workspace.openLinkText(path, '', false);
-				console.log("All done!");
-				return true;
-			}
-			return false;
-	}
-
-	renderTemplate(v: string) {
-		return v.replace(/{{\s*date\s*:\s*(.*?)}}/gi, (_, fmt) => {
-			return moment().format(fmt);
-		});
-	}
-
-	onunload() {
-		console.log('unloading new-file-templatte plugin');
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		let {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for new-file-template.'});
-
-		new Setting(containerEl)
-			.setName('Template')
-			.setDesc('Template string\n' + 'Date formats are supported {{date:YYYYMMDDHHmm}}')
-			.addTextArea(text => {
-						text.setPlaceholder(DEFAULT_TEMPLATE)
-								.setValue(this.plugin.settings.template)
-								.onChange(async (value) => {
-									console.log('Template string: ' + value);
-									this.plugin.settings.template = value;
-									await this.plugin.saveSettings();
-								});
-						text.inputEl.rows = 10;
-					}
-			);
-	}
+    new Setting(containerEl)
+      .setName("Template")
+      .setDesc(
+        "Template string\n" + "Date formats are supported {{date:YYYYMMDDHHmm}}"
+      )
+      .addTextArea((text) => {
+        text
+          .setPlaceholder(DEFAULT_TEMPLATE)
+          .setValue(this.plugin.settings.template)
+          .onChange(async (value) => {
+            console.log("Template string: " + value);
+            this.plugin.settings.template = value;
+            await this.plugin.saveSettings();
+          });
+        text.inputEl.rows = 10;
+      });
+  }
 }
